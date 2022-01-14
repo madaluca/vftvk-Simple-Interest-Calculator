224afb0986cd58171e336b1adad2fbdac8739afd
Madalina Luca <Madalina.Luca@nestle.com>
2022-01-14 19:52:51 +0200

    updates

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-}
-
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
 
+function compute()
+{   
+    if(principal < 1) {
+        alert("Enter a positive number")
+        return
+    }
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+}
+
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-}
-
-function checkPrincipal()
-{
-    if(principal < 1) alert("Enter a positive number")
 }
